0d893697c184f95dd15b5227afc741052f42b77b
Yeonny Kim <70524037+Yeonny0723@users.noreply.github.com>
2022-05-22 03:35:40 +1000

    Add files via upload

---
+++ digital_bus_sys/javascript_shopping_cart/js/simplecartjs-config.js
@@ -0,0 +1,7 @@
+simpleCart({
+    cartStyle: "table", // Display cart contents as a table.
+    currency: "AUD", // Use Australian dollars as the currency.
+    shippingFlatRate: 5.95, // Set a flat shipping rate of $5.95.
+    shippingQuantityRate: 1.00, // Set an additional shipping rate of $1.00 per product in cart
+    taxRate: 0.1, // Set a tax rate to reflect the Australian GST rate of 10%.
+});
