8c187bbd12fc48d6f851a53ce1696c5e06dc4527
Yeonny Kim <70524037+Yeonny0723@users.noreply.github.com>
2022-05-22 03:34:23 +1000

    Add files via upload

---
+++ digital_bus_sys/javascript_shopping_cart/logout.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Logout</h1>
+    <?php
+        session_start();
+        $_SESSION = array();
+        session_destroy();
+    ?>
+    
+    <a href="login-test.php">Login</a>
+</body>
+</html>
